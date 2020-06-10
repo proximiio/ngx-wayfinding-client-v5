191153d2149368ff804ad2dc779227d88ac24df0
Matej Drlicka <sprattek@gmail.com>
2020-06-10 11:23:44 +0200

    token change

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 var Settings = {
-  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImlzcyI6IjBjNGYyNjhmLTZjMmYtNDM5Zi1iMTI3LTMzYTcwZGY2MmMwZSIsInR5cGUiOiJ1c2VyIiwidXNlciI6Ik1hcmlhIFRlc3QgTWlrYSIsInVzZXJfaWQiOiJlYzQxZDhjMi1jMDJjLTRiZjktYjllYy0zYTRhMmY3MDhkMzMiLCJ0ZW5hbnRfaWQiOiIwYzRmMjY4Zi02YzJmLTQzOWYtYjEyNy0zM2E3MGRmNjJjMGUifQ.SpmhvgXw0fbjRhebvqf9L_8II4V6PV-4Xe4DpEs39Zo",
+  token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImlzcyI6IjI5NWIwYzFkLTNmYWMtNGUyZC05MWZlLTgxY2Y5MTI0ZTRiYSIsInR5cGUiOiJhcHBsaWNhdGlvbiIsImFwcGxpY2F0aW9uX2lkIjoiMDM3YWJhZTAtNWExMy00YzkyLTgxODEtZTY0YTRmNmFkYjI5In0.-nMemM6dCQyUrTBJVYDV-r0zTuZvuVfeGcCEs8h3ooo",
   basepath: "/maria01-v5", // in case of change it's required to change that also at ./src/environments and ./angular.json files
   port: "6004", // in case of change it's required to change that also at ./src/environments
   proximi_api: 'https://api.proximi.fi',
