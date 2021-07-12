ef8ee3becc4878dfa70ffe525af153f7a2dc1d11
Matej Drlicka <sprattek@gmail.com>
2021-07-12 10:33:42 +0200

    builded

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authUrl: '/wayfinding',
-  ahoyUrl: '/wayfinding/analytics',
+  authUrl: '/wayfinding-demo',
+  ahoyUrl: '/wayfinding-demo/analytics',
   appPrefix: 'ngx-wayfinding-'
 };
